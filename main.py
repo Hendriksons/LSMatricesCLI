043cfd0ac2ee1938865e97ac13925e5dba90fb5b
RimUwU <95649328+Hendriksons@users.noreply.github.com>
2025-05-15 08:22:12 +0000

    Adding Conversion of Matrix to Tex string

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Main:
                 resultOfRow += variableList[i] * listofXvars[i]
             variableList.append(resultOfRow)
         return variableList
+
     
     @classmethod
     def makeMatrics(self,amountOfVars:int,amountOfDigits:int):
@@ -20,6 +21,8 @@ class Main:
         for i in range(amountOfVars):
             rows.append(self.generateVariables(amountOfVars, amountOfDigits, rows[0]))
         return rows
+
+
     @classmethod
     def renderMatrics(self,amountOfDigits:int, amountOfVars:int):
         rows = self.makeMatrics(amountOfVars, amountOfDigits)
@@ -33,11 +36,35 @@ class Main:
                     matricsString += "|"
                 matricsString += " " + str(e) + " "
         print(matricsString)
+        print(self.listToMatrics(rows))
         return XValues
+
+
     @classmethod
     def start(self):
         result = self.renderMatrics(int(input("How many Digit should each Variable have?\n")), int(input("How many Variables should the Matrics have? \n\n\n")))
         input("")
         print(result) 
+
+
+    @classmethod
+    def listToMatrics(self,matricList) -> str:
+        s_latexMatrics: str = "{"
+        for i in matricList:
+            s_latexMatrics += "{"
+            for e in i:
+                s_latexMatrics += str(e)
+                if i.index(e) != (len(i)-1):
+                    s_latexMatrics += ","
+            s_latexMatrics += "}" 
+            if matricList.index(i) is not (len(matricList)-1):
+                s_latexMatrics += ","
+
+        return (s_latexMatrics + "}")
+
+
+
+
+
 if __name__ == "__main__":
     Main.start()
